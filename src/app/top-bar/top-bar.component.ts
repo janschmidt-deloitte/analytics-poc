ed7ac57b78340d6b4f5a652c993e78aac9aed3f7
janschmidt-deloitte <janschmidt@deloitte.de>
2019-11-05 16:11:01 +0100

    adjusted global stylesheet and fixed formatting

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { GoogleAnalyticsService } from '../google-analytics.service';
-import { MatomoTracker } from 'ngx-matomo';
+import { GoogleAnalyticsService } from "../google-analytics.service";
+import { MatomoTracker } from "ngx-matomo";
 
 @Component({
   selector: "app-top-bar",
@@ -8,15 +8,22 @@ import { MatomoTracker } from 'ngx-matomo';
   styleUrls: ["./top-bar.component.css"]
 })
 export class TopBarComponent implements OnInit {
+  constructor(
+    private googleAnalyticsService: GoogleAnalyticsService,
+    private matomoTracker: MatomoTracker
+  ) {}
 
-    constructor(private googleAnalyticsService: GoogleAnalyticsService, private matomoTracker: MatomoTracker) { }
+  ngOnInit() {}
 
-    ngOnInit() {}
-
-    cart() {
+  cart() {
     console.log("User clicked on cart!");
-    this.googleAnalyticsService.eventEmitter("add_to_cart", "shop", "cart", "click", 1);
-    this.matomoTracker.trackEvent('Ecommerce', 'add_to_cart', 'click', 1);
+    this.googleAnalyticsService.eventEmitter(
+      "add_to_cart",
+      "shop",
+      "cart",
+      "click",
+      1
+    );
+    this.matomoTracker.trackEvent("Ecommerce", "add_to_cart", "click", 1);
   }
-
-  }
+}
