ccf6e0321689ff62fdfabeeb688ae53b98b5e206
janschmidt-deloitte <janschmidt@deloitte.de>
2019-11-05 15:43:40 +0100

    fixed google analytics service and expanded wrapper function

--- src/app/google-analytics.service.ts
+++ src/app/google-analytics.service.ts
@@ -1,21 +1,26 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-declare let gtag: Function;
+declare let gtag:Function;
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class GoogleAnalyticsService {
+
+  constructor() { }
+
   public eventEmitter(
     eventName: string,
     eventCategory: string,
     eventAction: string,
     eventLabel: string = null,
     eventValue: number = null
-  ) {
-    gtag("event", eventName, {
+  ){
+    gtag('event', eventName, {
       eventCategory: eventCategory,
       eventLabel: eventLabel,
       eventAction: eventAction,
       eventValue: eventValue
-    });
+    })
   }
 }
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { GoogleAnalyticsService } from '../google-analytics.service';
 
 @Component({
   selector: "app-top-bar",
@@ -6,9 +7,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./top-bar.component.css"]
 })
 export class TopBarComponent implements OnInit {
-  log(message) {
-    console.log(message);
+
+    constructor(private googleAnalyticsService: GoogleAnalyticsService) { }
+
+    ngOnInit() {}
+
+    cart() {
+    console.log("User clicked on cart!");
+    this.googleAnalyticsService.eventEmitter("add_to_cart", "shop", "cart", "click", 1);
   }
 
-  ngOnInit() {}
-}
+  }
