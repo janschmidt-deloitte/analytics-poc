e2cbb52f91272dd19ad52e0f1239d9de4bb0044b
Schmidt <janschmidt@deloitte.de>
2019-11-05 13:02:28 +0100

    removed superfluous copyright notices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,10 +31,3 @@ import { CartComponent } from './cart/cart.component';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
