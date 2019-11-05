bec2504abad04db7481bfe91ec2c578af571d75d
janschmidt-deloitte <janschmidt@deloitte.de>
2019-11-05 15:55:49 +0100

    added matomo call to wrapper function

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { GoogleAnalyticsService } from '../google-analytics.service';
+import { MatomoTracker } from 'ngx-matomo';
 
 @Component({
   selector: "app-top-bar",
@@ -8,13 +9,14 @@ import { GoogleAnalyticsService } from '../google-analytics.service';
 })
 export class TopBarComponent implements OnInit {
 
-    constructor(private googleAnalyticsService: GoogleAnalyticsService) { }
+    constructor(private googleAnalyticsService: GoogleAnalyticsService, private matomoTracker: MatomoTracker) { }
 
     ngOnInit() {}
 
     cart() {
     console.log("User clicked on cart!");
     this.googleAnalyticsService.eventEmitter("add_to_cart", "shop", "cart", "click", 1);
+    this.matomoTracker.trackEvent('Ecommerce', 'add_to_cart', 'click', 1);
   }
 
   }
